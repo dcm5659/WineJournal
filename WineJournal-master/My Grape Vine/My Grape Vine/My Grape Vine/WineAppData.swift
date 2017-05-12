fa1f40c4f261ccdb7e7d621558d9ede6c683ab05
bxc1712 <bxc1712@rit.edu>
2017-05-12 17:28:26 -0400

    Added SiftyJSON Library
    For parsing api stuff

---
+++ WineJournal-master/My Grape Vine/My Grape Vine/My Grape Vine/WineAppData.swift
@@ -0,0 +1,15 @@
+//
+//  WineAppData.swift
+//  My Grape Vine
+//
+//  Created by Daniel Martin (RIT Student) on 4/26/17.
+//  Copyright © 2017 Daniel Martin (RIT Student). All rights reserved.
+//
+
+import Foundation
+
+class WineData {
+    static let sharedData = WineData()
+
+    private init(){}
+}
