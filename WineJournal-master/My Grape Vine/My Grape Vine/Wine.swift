ca1c6c9d1276740aaae9553d8b617fba6481fed4
Daniel Martin <dcm5659@rit.edu>
2017-05-15 21:07:49 -0400

    Add files via upload

---
+++ WineJournal-master/My Grape Vine/My Grape Vine/Wine.swift
@@ -0,0 +1,14 @@
+//
+//  Wine.swift
+//  My Grape Vine
+//
+//  Created by Daniel Martin (RIT Student) on 5/15/17.
+//  Copyright © 2017 Daniel Martin (RIT Student). All rights reserved.
+//
+
+import Foundation
+
+class Wine{
+//    var name:String
+//    var:
+}
